132a715a5e1d846fd1be70bd6060c5f686cc9239
Mark Aksen <mja2213@columbia.edu>
2020-12-16 20:11:10 -0500

    rearranged read and cleaning functions

---
+++ clean_data.py
@@ -0,0 +1,388 @@
+import pandas as pd
+from nltk import pos_tag
+from nltk.corpus import wordnet
+import gensim
+import spacy
+import time
+import re
+import itertools
+import gensim.corpora as corpora
+import collections
+import os
+import pickle
+
+def get_word_postag(word):
+    #if pos_tag([word])[0][1].startswith('J'):
+    #    return wordnet.ADJ
+    #if pos_tag([word])[0][1].startswith('V'):
+    #    return wordnet.VERB
+    if pos_tag([word])[0][1].startswith('N'):
+        #return wordnet.NOUN
+        return True
+    else:
+        return False
+        #return wordnet.ADJ
+        #return wordnet.NOUN
+
+from nltk.tokenize import word_tokenize
+# Preprocessing: tokenize words
+def tokenize(text):
+    return(word_tokenize(text))
+
+
+def sent_to_words(sentences):
+    for sentence in sentences:
+        return(gensim.utils.simple_preprocess(str(sentence), min_len=3,deacc=True))  # deacc=True removes punctuations
+
+from nltk.corpus import stopwords
+stopwords = stopwords.words('english')
+
+# Preprocessing: remove stopwords
+def remove_stopwords(text):
+    return ' '.join([word for word in text.split() if word not in stopwords]) 
+    #return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+
+# Preprocessing: lemmatizing
+nlp = spacy.load('en', disable=['parser', 'ner'])
+
+def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    """https://spacy.io/api/annotation"""
+    texts_out = []
+    for sent in texts:
+        doc = nlp(" ".join(sent)) 
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    return texts_out
+
+# Preprocessing: remove short text
+def find_longer_text(texts,k=200):
+    return list(map(lambda x: len(x.split())>k,texts))
+    
+#     lengths = list(map(lambda x: len(x.split()), texts))
+#     return [val >= k for val in lengths]
+    #return [idx for idx, val in enumerate(lengths) if val >= k] 
+
+# Preprocessing: alpha num
+def keep_alphanum(words):
+    #def isalphanum(word):
+    #return word.isalnum()
+    return filter(lambda word: word.isalnum(), words)
+    #return [word for word in words if word.isalnum()]
+
+# Preprocessing: keep nouns
+def keep_nouns(words):
+    return filter(get_word_postag, words)
+    #return [word for word in words if get_word_postag(word) =='n']
+
+# Preprocessing: keep words >= 3 in length
+def keep_longer_words(words):
+    return list(filter(lambda x: (len(x) >= 3), words))
+    #return [word for word in words if len(word) >= 3]
+
+# Preprocessing: lemmatize
+from nltk.stem import WordNetLemmatizer
+lm = WordNetLemmatizer()
+def lemmatize(words):
+    return list(map(lm.lemmatize, words)) 
+
+# Preprocessing: stemming
+from nltk.stem import PorterStemmer 
+ps = PorterStemmer() 
+def stemming(words):
+    #return [ps.stem(word) for word in words]
+    return map(ps.stem, words)
+
+def remove_digits(words):
+    return list(filter(lambda x: x.isalpha(), words))
+    #return list(filter(lambda x: x.isalpha(), words))
+#     return [word for word in words if word.isalpha()]
+
+def merged(words):
+    return ' '.join(word for word in words)   
+
+def clean_pdf(text_df, file_name='', output_dir='',section_lvl = False):
+    
+    start = time.time()
+    
+
+    # if index is not paper_id
+    if text_df.index.name is None: 
+        print('changing index to paper_id')
+        text_df = text_df.set_index('paper_id')
+
+    ids = text_df.index.values.astype(str)
+        
+    contents = text_df['whole_text'].values.tolist()
+    
+    # Add abstract to text
+    if not section_lvl:
+        abstracts = text_df['abstract'].values.tolist()
+
+        contents = [i + j for i, j in zip(contents, abstracts)]
+    
+    t = time.time()
+    print(t-start)
+    
+    # Remove new line characters
+    contents = map(lambda x: re.sub('\s+', ' ', x), contents)
+    
+    t = time.time()
+    print(t-start)
+    
+    # Preprocessing: lower case text
+    contents = map(lambda x: x.lower(),contents)
+    
+    t = time.time()
+    print(t-start)
+    
+    # Preprocessing: keep alphanumeric
+    contents = map(lambda x: re.sub(r'[^A-Za-z0-9 ]+', '', x), contents)
+    
+    t = time.time()
+    print(t-start)
+    
+    # Preprocessing: remove stand along numbers
+    contents = map(lambda x: re.sub(" \d+ ", " ", x), contents)
+
+    t = time.time()
+    print(t-start)
+    
+    # Preprocessing: remove stop words
+    contents = map(remove_stopwords, contents)
+    
+    t = time.time()
+    print(t-start)
+    
+    contents = list(contents)
+
+    # Preprocessing: remove short text
+    inds = find_longer_text(contents)
+    contents = itertools.compress(contents, inds)
+    ids = list(itertools.compress(ids, inds))
+    
+    key_words = text_df.loc[ids]['key_words'].values
+    print('Tokenizing')
+    
+    # Tokenize words + remove punctuation
+    tokenized_contents = map(tokenize,contents) # documents in BOW format
+#     word_list = [tokenize(article) for article in contents]
+    t = time.time()
+    print(t-start)
+    
+    # Remove numbers
+    tokenized_contents = map(remove_digits, tokenized_contents)
+    
+    
+    t = time.time()
+    print(t-start)
+    
+    # Keep longer words
+#     word_list = [keep_longer_words(words) for words in  word_list]
+    tokenized_contents = map(keep_longer_words,  tokenized_contents)
+    
+    
+    t = time.time()
+    print(t-start)
+    
+    print('Lemmatizing')
+    
+    # Preprocessing: lemmatize
+    tokenized_contents = map(lemmatize, tokenized_contents)
+    
+#     print(list(word_list)[0])
+    
+    t = time.time()
+    print(t-start)
+    
+    print('Bag of Words Representation')
+    tokenized_contents = list(tokenized_contents)
+
+    dct = corpora.Dictionary(tokenized_contents) # make dct before corpus
+#     doc2bow = partial(dct.doc2bow,allow_update=True)
+    
+    print('length of dct before filter_extreme: ', len(dct))
+    dct.filter_extremes() # using default params # filter dct before creating corpus
+    print('length of dct after filter_extreme: ', len(dct))
+    
+
+    # Make corpus after any changes to dct
+    corpus = list(map(lambda x: dct.doc2bow(x,allow_update=True), tokenized_contents))    
+#     corpus = [dct.doc2bow(doc, allow_update=True) for doc in word_list]
+
+    t = time.time()
+    print(t-start)
+
+
+    
+    word_list =  [item for sublist in tokenized_contents for item in sublist]
+    counter=collections.Counter(word_list)
+    
+    if file_name:
+        os.makedirs(output_dir, exist_ok=True)
+        output_file_name = output_dir + file_name + '_clean.pkl'
+
+
+        with open(output_file_name, 'wb') as f:  # Python 3: open(..., 'wb') 
+            d = {'dct': dct, 'corpus': corpus, 'docs': tokenized_contents, 
+                 'counter': counter, 'ids': ids, 'word_list':word_list,
+                 'key_words':key_words}
+            pickle.dump(d, f)
+    return d
+
+def process_sections(textdf, file_name=''):
+    def merge_sections(doc):
+        sections = doc['body_text']
+        #print(len(sections))
+        #print(sections[0])
+        if type(sections) is list and len(sections)>0:
+            section_titles = [x['section'] for x in sections]
+            paper_id = doc['paper_id']
+            texts = []; titles = [];
+            current_text = sections[0]['text']
+            current_title = section_titles[0]
+            for i in range(1,len(sections)):
+                #print(i)
+                if section_titles[i] == current_title:
+                    current_text = current_text + ' ' + sections[i]['text']
+                else: 
+                    texts.append(current_text)
+                    titles.append(current_title)
+                    current_title = section_titles[i]
+                    current_text = sections[i]['text']
+            sections_df = pd.DataFrame({'whole_text': texts, 'titles':titles, 'paper_id':paper_id})
+            return sections_df
+        else:
+            return []
+    total_df = pd.DataFrame(columns = ['whole_text', 'titles', 'paper_id'])
+    for index,row in textdf.iterrows():
+        #print(index)
+        
+        total_df = total_df.append(merge_sections(row))
+    return total_df
+
+def clean_section(text_df, file_name='', output_dir='',section_lvl = False):
+    
+    start = time.time()
+    
+
+    # if index is not paper_id
+    if text_df.index.name is None: 
+        print('changing index to paper_id')
+        text_df = text_df.set_index('paper_id')
+
+    ids = text_df.index.values.astype(str)
+        
+    contents = text_df['whole_text'].values.tolist()
+    
+    # Add abstract to text
+    if not section_lvl:
+        abstracts = text_df['abstract'].values.tolist()
+
+        contents = [i + j for i, j in zip(contents, abstracts)]
+    
+    t = time.time()
+    print(t-start)
+    
+    # Remove new line characters
+    contents = map(lambda x: re.sub('\s+', ' ', x), contents)
+    
+    t = time.time()
+    print(t-start)
+    
+    # Preprocessing: lower case text
+    contents = map(lambda x: x.lower(),contents)
+    
+    t = time.time()
+    print(t-start)
+    
+    # Preprocessing: keep alphanumeric
+    contents = map(lambda x: re.sub(r'[^A-Za-z0-9 ]+', '', x), contents)
+    
+    t = time.time()
+    print(t-start)
+    
+    # Preprocessing: remove stand along numbers
+    contents = map(lambda x: re.sub(" \d+ ", " ", x), contents)
+
+    t = time.time()
+    print(t-start)
+    
+    # Preprocessing: remove stop words
+    contents = map(remove_stopwords, contents)
+    
+    t = time.time()
+    print(t-start)
+    
+    contents = list(contents)
+
+    # Preprocessing: remove short text
+    inds = find_longer_text(contents)
+    contents = itertools.compress(contents, inds)
+    ids = list(itertools.compress(ids, inds))
+    
+    #key_words = text_df.loc[ids]['key_words'].values
+    print('Tokenizing')
+    
+    # Tokenize words + remove punctuation
+    tokenized_contents = map(tokenize,contents) # documents in BOW format
+#     word_list = [tokenize(article) for article in contents]
+    t = time.time()
+    print(t-start)
+    
+    # Remove numbers
+    tokenized_contents = map(remove_digits, tokenized_contents)
+    
+    
+    t = time.time()
+    print(t-start)
+    
+    # Keep longer words
+#     word_list = [keep_longer_words(words) for words in  word_list]
+    tokenized_contents = map(keep_longer_words,  tokenized_contents)
+    
+    
+    t = time.time()
+    print(t-start)
+    
+    print('Lemmatizing')
+    
+    # Preprocessing: lemmatize
+    tokenized_contents = map(lemmatize, tokenized_contents)
+    
+#     print(list(word_list)[0])
+    
+    t = time.time()
+    print(t-start)
+    
+    print('Bag of Words Representation')
+    tokenized_contents = list(tokenized_contents)
+
+    dct = corpora.Dictionary(tokenized_contents) # make dct before corpus
+#     doc2bow = partial(dct.doc2bow,allow_update=True)
+    
+    print('length of dct before filter_extreme: ', len(dct))
+    dct.filter_extremes() # using default params # filter dct before creating corpus
+    print('length of dct after filter_extreme: ', len(dct))
+    
+
+    # Make corpus after any changes to dct
+    corpus = list(map(lambda x: dct.doc2bow(x,allow_update=True), tokenized_contents))    
+#     corpus = [dct.doc2bow(doc, allow_update=True) for doc in word_list]
+
+    t = time.time()
+    print(t-start)
+
+
+    
+    word_list =  [item for sublist in tokenized_contents for item in sublist]
+    counter=collections.Counter(word_list)
+    
+    if file_name:
+        os.makedirs(output_dir, exist_ok=True)
+        output_file_name = output_dir + file_name + '_clean.pkl'
+
+
+        with open(output_file_name, 'wb') as f:  # Python 3: open(..., 'wb') 
+            d = {'dct': dct, 'corpus': corpus, 'docs': tokenized_contents, 
+                 'counter': counter, 'ids': ids, 'word_list':word_list}
+            pickle.dump(d, f)
+    return d
---
+++ read_data.py
@@ -0,0 +1,206 @@
+import numpy as np
+import pandas as pd
+import io
+import json
+import gzip
+from tqdm import tqdm
+import os
+import pickle
+
+def process_metadata(metadata_file,fields=None, get_ids=None,output_dir = './processed/',put_in_op_dir=False):
+    '''
+    
+    
+
+    Parameters
+    ----------
+    metadata_file : TYPE
+        DESCRIPTION.
+    fields : TYPE, optional
+        DESCRIPTION. The default is None.
+    get_ids : TYPE, optional
+        DESCRIPTION. The default is None.
+    output_dir : TYPE, optional
+        DESCRIPTION. The default is './processed/'.
+    put_in_op_dir : TYPE, optional
+        DESCRIPTION. The default is False.
+
+    Returns
+    -------
+    meta_df_file : TYPE
+        DESCRIPTION.
+
+    '''
+    file_name_without_path_or_ext = metadata_file.split('/')[-1].split('.')[0]
+    # Go through metadata files to get relevant paper ids and titles
+    ids = []; title = []; field = [];
+    # if file is compressed
+    if metadata_file[-3:] == '.gz':
+        if put_in_op_dir:
+            output_file = output_dir + file_name_without_path_or_ext
+        else:  
+            output_file = metadata_file[:-3]
+        gz = gzip.open(metadata_file, 'rb')
+        f = io.BufferedReader(gz)
+        f_out = open(output_file,'wb')
+    else:
+        f = open(metadata_file)
+        f_out = None
+
+    for line in tqdm(f.readlines()):
+        paper = json.loads(line)
+        if not fields:
+            if not get_ids:
+                ids.append(paper['paper_id'])
+                title.append(paper['title'])
+                field.append(paper['mag_field_of_study'])
+                if f_out:
+                    f_out.write(line)
+            elif paper['paper_id'] in get_ids:
+                ids.append(paper['paper_id'])
+                title.append(paper['title'])
+                field.append(paper['mag_field_of_study'])
+                if f_out:
+                    f_out.write(line)
+        elif paper['mag_field_of_study']:
+            field_in = any([x in fields for x in paper['mag_field_of_study']])
+            if field_in:
+                ids.append(paper['paper_id'])
+                title.append(paper['title'])
+                field.append(paper['mag_field_of_study'])
+                if f_out:
+                    f_out.write(line)
+    f.close()
+    if f_out:
+        f_out.close()
+    # create and save dataframe in output_dir/meta_df
+    meta_df = pd.DataFrame({'ids':ids, 'titles':title, 'field': field})
+    meta_df_dir = output_dir + 'meta_df/'
+    os.makedirs(meta_df_dir, exist_ok=True)
+    meta_df_file = meta_df_dir + file_name_without_path_or_ext + '.pkl'
+    with open(meta_df_file, 'wb') as f:
+        pickle.dump(meta_df, f)
+    return meta_df_file
+
+def process_pdf(meta_df_file, pdf_file,fields=None,get_ids=None, output_dir = './processed/'):
+    '''
+    
+
+    Parameters
+    ----------
+    meta_df_file : TYPE
+        DESCRIPTION.
+    pdf_file : TYPE
+        DESCRIPTION.
+    fields : TYPE, optional
+        DESCRIPTION. The default is None.
+    get_ids : TYPE, optional
+        DESCRIPTION. The default is None.
+    output_dir : TYPE, optional
+        DESCRIPTION. The default is './processed/'.
+
+    Returns
+    -------
+    text_df_file : TYPE
+        DESCRIPTION.
+
+    '''
+    
+    # use meta data df to check ids
+    with open(meta_df_file, 'rb') as f:
+        meta_df = pickle.load(f)
+    # get the pdfs 
+    # lists to make pdf dataframe from 
+    papers_ids_text = []; abstract = [] 
+    body_text = []  # list of dicts (for each paper) with section, text, cite_spans
+    whole_text = [] # list of strings (for each paper) of entire text in the body
+    key_words = []  # key words mentioned with the abstract
+    citations = [] 
+    # if file is compressed
+    if pdf_file[-3:] == '.gz':
+        output_file = pdf_file[:-3]
+        gz = gzip.open(pdf_file, 'rb')
+        f = io.BufferedReader(gz)
+        f_out = open(output_file,'wb')
+    else:
+        f = open(pdf_file)
+        f_out = None
+        
+    for line in tqdm(f.readlines()):
+        paper = json.loads(line)
+        if paper['paper_id'] in meta_df['ids'].values: #ids defined in meta data df based on field
+            if f_out: # if untaring
+                f_out.write(line) # write that pdf (untarred pdf parse will only have selected papers)
+            # get paper-id
+            papers_ids_text.append(paper['paper_id'])
+            abstract_text = ''
+            terms = []
+            # get abstract
+            if paper['abstract']:
+                abstract_text = paper['abstract'][0]['text']
+                # get key_words in abstract
+                if len(paper['abstract'])>1:   
+                    if paper['abstract'][1]['text'][:11].lower() == 'index terms':
+                        terms = paper['abstract'][1]['text'][12:].split(',') #remove "Index Terms-" or "INDEX TERMS " from string    
+            abstract.append(abstract_text) 
+            key_words.append(terms)
+            # get sections and text from body
+            text = []
+            full_text = ''
+            if paper['body_text']:
+                for entry in paper['body_text']:
+                    if entry['section'] and entry['text']:
+                        section = {key: entry[key] for key in ['section', 'text', 'cite_spans']}
+                        text.append(section)
+                        if full_text: # why the if-else?
+                            full_text = full_text + '\n' + entry['text']
+                        else:
+                            full_text = entry['text']
+            body_text.append(text)
+            whole_text.append(full_text)
+            # get citations
+            bib_entries = {}
+            if paper['bib_entries']:
+                bib_entries = paper['bib_entries']
+            citations.append(bib_entries)
+            
+    f.close()
+    if f_out:
+        f_out.close()
+    # create and save dataframe in output_dir/text_df
+    text_df = pd.DataFrame({'paper_id': papers_ids_text, 'abstract': abstract,'key_words': key_words,'body_text': body_text, 'whole_text': whole_text,'citations':citations})
+    text_df_dir = output_dir + 'text_df/'
+    os.makedirs(text_df_dir, exist_ok=True)
+    file_name_without_path_or_ext = pdf_file.split('/')[-1].split('.')[0]
+    text_df_file = text_df_dir + file_name_without_path_or_ext + '.pkl'
+    with open(text_df_file, 'wb') as f:
+        pickle.dump(text_df, f)         
+    return text_df_file       
+
+def process_batch(metadata_file,pdf_file, fields=None, get_ids=None, output_dir = './processed/'):
+    '''
+    
+    Parameters
+    ----------
+    metadata_file : TYPE
+        DESCRIPTION.
+    pdf_file : TYPE
+        DESCRIPTION.
+    fields : TYPE, optional
+        DESCRIPTION. The default is None.
+    get_ids : TYPE, optional
+        DESCRIPTION. The default is None.
+    output_dir : TYPE, optional
+        DESCRIPTION. The default is './processed/'.
+
+    Returns
+    -------
+    meta_df_file : TYPE
+        DESCRIPTION.
+    text_df_file : TYPE
+        DESCRIPTION.
+
+    '''
+    meta_df_file = process_metadata(metadata_file,fields,get_ids,output_dir)
+    text_df_file = process_pdf(meta_df_file,pdf_file,fields,get_ids,output_dir)
+    return meta_df_file,text_df_file
